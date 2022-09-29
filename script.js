f7034cf348c6f21f545c463987e3ecff7f596568
Vinicius <vinicius_henrique96@hotmail.com>
2022-09-29 12:50:16 -0300

    Implementando o carregamento lento de imagens para melhor desempenho

--- script.js
+++ script.js
@@ -213,6 +213,34 @@ allSections.forEach(section => {
 });
 
 /////////////////////////////////////
+/// Lazy Loading Images
+const imgTargets = document.querySelectorAll('img[data-src');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  // usamos esse evento pra quando o javascript terminar de carregar a imagem, removemos a classe
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+/////////////////////////////////////
 /// Selecting, Creating, and Deleting Elements
 
 // console.log(document.documentElement);
